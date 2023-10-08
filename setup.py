d6ae41e1812dec5d0354e7e0d7ffdb16e6790804
HaroldConley <conley.harold@gmail.com>
2023-10-08 18:46:40 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recomendador de peliculas basado en plots',
+    author='Harold_Conley',
+    license='',
+)
