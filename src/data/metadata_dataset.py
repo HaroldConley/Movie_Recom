3046d5e182215b12e733eab18aa8b8f1523f2c5e
HaroldConley <conley.harold@gmail.com>
2023-10-09 15:31:25 -0300

    Actualización de códigos para modificar la base de datos (funciones, descargas, etc.)

--- src/data/metadata_dataset.py
+++ src/data/metadata_dataset.py
@@ -1,7 +1,26 @@
 # Código para generar el DB de trabajo, teniendo en mente lo descubierto en el EDA (Jupyter notebook).
 
 import pandas as pd
-from name_cleaner import eliminar_nombres
+import spacy
+
+# Cargar modelo NLP
+nlp = spacy.load("en_core_web_trf")
+
+
+# Función para eliminar nombres de personas de un texto
+def eliminar_nombres(texto):
+    """ Función para eliminar los nombres de personas de un texto dado.
+
+    :param texto: el texto de donde se eliminarán los nombres.
+    :return: texto sin los nombres.
+
+    >>> eliminar_nombres('Mi nombre es John Connor, lider de la rebelión.')
+    'Mi nombre es  , lider de la rebelión.'
+    """
+    doc = nlp(texto)
+    palabras_sin_nombres = [token.text for token in doc if token.ent_type_ != "PERSON"]
+    return " ".join(palabras_sin_nombres)
+
 
 # Importar data raw
 movies = pd.read_csv('../../data/raw/0_inicial/movies.csv')
